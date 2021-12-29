6ffdb2fe7c91d7fbe77bd914fe784d5672cdd3f7
Ankit Negi <ankitn1311@gmail.com>
2021-12-29 18:28:48 +0530

    new template file

--- template.cpp
+++ template.cpp
@@ -1,9 +1,13 @@
 #include<bits/stdc++.h>
-#define ll long long
+
+using namespace std;
+
 #define pb push_back
 #define fr(a,b) for(int i = a; i < b; i++)
 #define rep(i,a,b) for(int i = a; i < b; i++)
-#define mod 1000000007
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
 #define inf (1LL<<60)
 #define all(x) (x).begin(), (x).end()
 #define prDouble(x) cout << fixed << setprecision(10) << x
@@ -11,13 +15,39 @@
 #define goog(tno) cout << "Case #" << tno <<": "
 #define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
 #define read(x) int x; cin >> x
-using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) { cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}"; }
+template <class T> void _print(vector <T> v) { cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(set <T> v) { cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(multiset <T> v) { cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template <class T, class V> void _print(map <T, V> v) { cerr << "[ "; for (auto i : v) { _print(i); cerr << " "; } cerr << "]"; }
 
 void init_code() {
 	fast_io;
 #ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
 #endif 
 }
 
