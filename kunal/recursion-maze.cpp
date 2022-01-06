76c1144d1ddb12eeba2758472cb77680fdf0bffd
Ankit Negi <ankitn1311@gmail.com>
2022-01-06 12:45:18 +0530

    recursion maze problems

---
+++ kunal/recursion-maze.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define fr(a,b) for(int i = a; i < b; i++)
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define inf (1LL<<60)
+#define all(x) (x).begin(), (x).end()
+#define prDouble(x) cout << fixed << setprecision(10) << x
+#define triplet pair<ll,pair<ll,ll>>
+#define goog(tno) cout << "Case #" << tno <<": "
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define read(x) int x; cin >> x
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) { cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}"; }
+template <class T> void _print(vector <T> v) { cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(set <T> v) { cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(multiset <T> v) { cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template <class T, class V> void _print(map <T, V> v) { cerr << "[ "; for (auto i : v) { _print(i); cerr << " "; } cerr << "]"; }
+
+void init_code() {
+	fast_io;
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif 
+}
+
+int maze(int row, int col) {
+	if (row == 1 || col == 1) {
+		return 1;
+	}
+	int left = maze(row - 1, col);
+	int right = maze(row, col - 1);
+	return left + right;
+}
+
+void mazeReturn(string path, int row, int col) {
+	if (row == 1 && col == 1) {
+		cout << path << " ";
+		return;
+	}
+
+
+	if (row > 1) {
+		mazeReturn(path + "D", row - 1, col);
+	}
+	if (col > 1) {
+		mazeReturn(path + "R", row, col - 1);
+	}
+
+}
+
+int main() {
+	init_code();
+	int t = 1;
+	// cin >> t;
+	while (t--) {
+		// cout << maze(10, 10);
+		mazeReturn("", 3, 3);
+	}
+	return 0;
+}
